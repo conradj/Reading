be8d23b9a47c9fa5e74342ee48b7b9f24bd8be20
conradj <conradsjackson@gmail.com>
2016-06-07 21:36:46 +0100

    Initial Pocket intgration

--- index.js
+++ index.js
@@ -1,16 +1,27 @@
-"use strict";
-require('dotenv-safe').load();
+"use strict"
+require('dotenv-safe').load()
 
-var express = require('express');
-var app = express();
+var express = require('express')
+var app = express()
+var pocket = require('./pocket')
 
 app.get('/', function (req, res) {
-  res.send('Hello World')
+    pocket.init(res)
+    
+})
+
+app.get('pocketAuth', function(req, res) {
+    pocket.init(res)
+})
+
+app.get('/list', function (req, res) {
+    pocket.finalAuth(res);
+    
 })
 
 // create server
 var port = process.env.PORT || 5000;
 
-app.listen(port, function() {
+app.listen(port, function () {
     console.log("Listening on " + port);
 })
---
+++ pocket.js
@@ -0,0 +1,91 @@
+/*
+authenticate app
+*/
+
+var GetPocket = require('node-getpocket');
+require('dotenv-safe').load()
+
+var Pocket = function () {
+    this.loginAttempted = false;
+
+    this.cfg = {
+        consumer_key: process.env.POCKET_CONSUMER_KEY,
+        request_token: '',
+        access_token: '',
+        user_name: '',
+        redirect_uri: process.env.OAUTH_REDIRECT_URL
+    };
+
+    this.pocketContext = new GetPocket(this.cfg);
+};
+
+Pocket.prototype.init = function (res) {
+    var self = this;
+    console.log(self.cfg);
+
+    console.log('Asking GetPocket for request token ...');
+    console.log('cfg: ', self.cfg);
+    self.pocketContext.getRequestToken(self.cfg, function (err, resp, body) {
+        this.loginAttempted = true;
+        if (err) {
+            console.log('Failed to get request token: ' + err);
+            res.send('<p>' + 'Failed to get request token: ' + err + '</p>');
+        } else if (resp.statusCode !== 200) {
+            res.send('<p>Oops, Pocket said ' + resp.headers.status + ', ' + resp.headers['x-error'] + '</p>');
+        } else {
+            var json = JSON.parse(body);
+            self.cfg.request_token = json.code;
+            console.log('Received request token: ' + self.cfg.request_token);
+
+            var url = self.pocketContext.getAuthorizeURL(self.cfg);
+            console.log('Redirecting to ' + url + ' for authentication');
+            res.redirect(url);
+        }
+    });
+}
+
+Pocket.prototype.finalAuth = function (res) {
+    var self = this;
+
+    console.log('Authentication callback active ...');
+    console.log('Asking GetPocket for access token ...');
+
+    console.log('cfg: ', self.cfg);
+    self.pocketContext.getAccessToken(self.cfg, function access_token_handler(err, resp, body) {
+        if (err) {
+            console.log('Failed to get access token: ' + err);
+            res.send('<p>' + 'Failed to get access token: ' + err + '</p>');
+        } else if (resp.statusCode !== 200) {
+            res.send('<p>Oops, Pocket said ' + resp.headers.status + ', ' + resp.headers['x-error'] + '</p>');
+        } else {
+            var json = JSON.parse(body);
+            self.cfg.access_token = json.access_token;
+            self.cfg.user_name = json.username;
+            console.log('Received access token: ' + self.cfg.access_token + ' for user ' + self.cfg.user_name);
+
+            // res.send('<p>Pocket says "yes"</p>' +
+            //     '<p>Your <code>GetPocket</code> configuration should look like this ...</p>' +
+            //     '<p><code>var cfg = ' + JSON.stringify(self.cfg, undefined, 2) + ';</code></p>');
+                
+            self.getWeek(res);
+        }
+    });
+}
+
+Pocket.prototype.getWeek = function (res) {
+    var self = this;
+    var params = {
+        "count":"1",
+        "state": "archive",
+        "detailType":"complete",
+        "sort": "newest"
+    };
+    self.pocketContext.get(params, function (err, resp) {
+        // check err or handle the response
+        console.log(err, resp.list)
+        
+        res.send("<div>" + JSON.stringify(resp, undefined, 2)  +  "</div>")
+    });
+}
+
+module.exports = new Pocket();
