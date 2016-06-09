81c333b87fa59b06371538786ac0b6c7603df760
conradj <conradsjackson@gmail.com>
2016-06-09 22:50:29 +0100

    further mongo

--- dataimport/userInit.js
+++ dataimport/userInit.js
@@ -6,20 +6,60 @@ var MongoClient = require('mongodb').MongoClient
 
 /// TODO loop through users?
 
-// get data from pocket for user
-var data = require("./conradj.json")
+
 
 // add to user collection
 // TODO: split by week?
 
 
 var url = 'mongodb://localhost:27017/reader'
-MongoClient.connect(url, function(err, db) {
-  if(err) {
-	console.warn("error!", err)
-  } else {
-    console.log("Connected correctly to server.")  
-  }
-  db.close()
-})
 
+
+function initNewUsers() {
+  var user, articles
+  // foreach new user in db
+  user = { name: "conradj" }
+  // get all of their read articles, 5000 at a time
+  articles = getUserArticles(user, 0)
+  console.log(articles.length)
+  insertAllArticlesForUser(user, articles)
+}
+
+function getUserArticles(user, offset) {
+  // connect to pocket under their credentials
+  var articles
+  var queryResults = require("./conradj.json");
+
+  var articles = Object.keys(queryResults.list).map(function (value, articleIndex) {
+    return queryResults.list[value]
+  })
+
+  if (articles.length >= 5000) {
+    // max number so there are likely to be more results to get
+    articles += getUserArticles(user, offset + 5000)
+  }
+  return articles
+}
+
+function insertAllArticlesForUser(user, articles) {
+  MongoClient.connect(url, function (err, db) {
+    if (err) {
+      console.warn("error!", err)
+    } else {
+      console.log("Connected correctly to server.")
+      var col = db.collection(user.name)
+      
+      col.insertMany(articles, function (err, r) {
+        if (err) {
+          console.warn("error on insert of many!", err)
+        } else {
+         console.log("inserted", r.insertedCount)
+         db.close()
+        }
+      })
+    }
+    
+  })
+}
+
+initNewUsers()
--- index.js
+++ index.js
@@ -6,6 +6,9 @@ var app = express();
 var session = require('express-session')
 var uid = require('uid-safe')
 var pocket = require('./pocket')
+var MongoClient = require('mongodb').MongoClient;
+var ObjectId = require('mongodb').ObjectID;
+var url = 'mongodb://localhost:27017/reader';
 
 app.use(session({
   genid: function(req) {
@@ -38,7 +41,14 @@ app.get('/list', function (req, res) {
 
 app.get('/conradj', function (req, res) {
     // get articles out of db
-    res.send("<div>Conradj</div>");
+    MongoClient.connect(url, function(err, db) {
+        findUserArticles(db, 'conradj', function (articles) {
+            console.log(articles)
+            res.send("<div>Conradj</div>")
+            db.close()
+        });
+    });
+    
 })
 
 // create server
@@ -47,3 +57,10 @@ var port = process.env.PORT || 5000;
 app.listen(port, function () {
     console.log("Listening on " + port);
 })
+
+
+var findUserArticles = function(db, username, callback) {
+   var cursor = db.collection(username).find()
+   
+   callback(cursor.toArray())
+};
